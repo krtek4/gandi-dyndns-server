b0ff1e751620ab4ec66ea511824a480145cb2634
Gilles Crettenand <gilles.crettenand@liip.ch>
2017-12-16 18:41:26 +0100

    use http status code

--- index.php
+++ index.php
@@ -21,17 +21,23 @@ function check_ip($ip) {
     return filter_var($ip, FILTER_VALIDATE_IP);
 }
 
+function error($code, $message) {
+    http_response_code($code);
+    header($message);
+    die($message);
+}
+
 openlog("DDNS-Provider", LOG_PID | LOG_PERROR, LOG_LOCAL0);
 
 $ip = check_ip(get_value('myip'));
 if($ip === false) {
-	$ip = check_ip(get_value('ip'));
+    $ip = check_ip(get_value('ip'));
 }
 if($ip === false) {
-	$ip = check_ip(get_value('dnsto'));
+    $ip = check_ip(get_value('dnsto'));
 }
 if($ip === false) {
-	$ip = check_ip(get_value('REMOTE_ADDR', [$_SERVER]));
+    $ip = check_ip(get_value('REMOTE_ADDR', [$_SERVER]));
 }
 if($ip === false) {
     syslog(LOG_WARNING, "No IP received.");
@@ -41,49 +47,47 @@ if($ip === false) {
 $user = get_value('PHP_AUTH_USER', [$_SERVER]);
 $pwd = get_value('PHP_AUTH_PW', [$_SERVER]);
 if($user === false || $pwd === false) {
-	$user = get_value('domain');
-	$pwd = get_value('password');
+    $user = get_value('domain');
+    $pwd = get_value('password');
 }
 if($user === false || $pwd === false) {
-	$user = get_value('token');
-	$pwd = '';
+    $user = get_value('token');
+    $pwd = '';
 }
 if($user === false || $pwd === false) {
     syslog(LOG_WARNING, " No token provided from $ip");
-    exit(2);
+    error(403, 'HTTP/1.0 403 Forbidden');
 }
 $token = $user.$pwd;
 $check = trim(file_get_contents('token'));
 if($token !== $check) {
     syslog(LOG_WARNING, "Token ($token $check) did not match for $user from $ip");
-    exit(2);
+    error(401, 'HTTP/1.0 401 Unauthorized');
 }
 
 $domain = get_value('hostname');
 if($domain === false || $domain === 'YES') {
-	$domain = get_value('host_id');
+    $domain = get_value('host_id');
 }
 if($domain === false) {
-	$domain = get_value('host');
+    $domain = get_value('host');
 }
 if($domain === false) {
     syslog(LOG_WARNING, "User $user from $ip didn't provide any domain");
-    exit(4);
+    error(400, 'HTTP/1.0 400 Bad request');
 }
 
 $parts = explode('.', $domain);
-
 $domain  = implode('.', array_slice($parts, -2, 2));
 $subdomain = implode('.', array_slice($parts, 0, -2));
-
-$config = file_get_contents('template.config.py');
 
 $secret = trim(file_get_contents('secret'));
 if(empty($secret)) {
     syslog(LOG_WARNING, "No secret found.");
-    exit(5);
+    error(501, 'HTTP/1.0 501 Not implemented');
 }
 
+$config = file_get_contents('template.config.py');
 file_put_contents('config.py', sprintf($config,
     $secret,
     $domain,
@@ -93,12 +97,13 @@ file_put_contents('config.py', sprintf($config,
 $out = exec('python gandi-live-dns.py', $output, $status);
 
 if($status !== 0) {
-	syslog(LOG_WARNING, "status: $status");
-	syslog(LOG_WARNING, "out: $out");
-	syslog(LOG_WARNING, "output: ".print_r($output, true));
+    syslog(LOG_WARNING, "status: $status");
+    syslog(LOG_WARNING, "out: $out");
+    syslog(LOG_WARNING, "output: ".print_r($output, true));
+    error(500, 'HTTP/1.0 500 Internal server error');
 }
 
 closelog();
 
-exit($status);
-
+http_response_code(200);
+echo "good\n";
